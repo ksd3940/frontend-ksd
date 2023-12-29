ea144c4a99c849d53f07c5eadbeb4beb6dfa2315
ksd3940 <ksd3940@naver.com>
2023-12-29 14:55:25 +0900

    2023/12/20_보강

---
+++ E_react0912/src/views/1229/React/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Async from './Async';
+import ReactGrammar from './ReactGrammar';
+
+export default function Index() {
+  return (
+    <>
+      <ReactGrammar />
+      <br />
+      <Async />
+    </>
+  );
+}
